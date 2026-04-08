b9b2513fe29534d3de34242d3a3d9d79b7db5a55
aryan <your-email@example.com>
2026-04-08 23:10:04 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def home():
+    return {"message": "Collabify API running"}
